94a8abe226de074dc9276e7e2fbbb3497d5f0cd9
amandalocher <akent22000@gmail.com>
2024-12-19 10:54:52 -0500

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 3, 'M', 'Brown', 'Nice', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Rowdy', 4, 'M', 'Black', 'Happy', False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Tabitha', 5, 'F', 'Brown', 'Nice', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Sergeant Brown', 6, 'M', 'Black', 'Happy', False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Melissa', 7, 'F', 'Brown', 'Nice', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Grinch', 8, 'M', 'Black', 'Happy', False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Wendy', 9, 'F', 'Brown', 'Nice', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, 'M', 'Black', 'Happy', True);
+
+-- INSERT INTO bears (name, age, breed) VALUES 
+-- (Mr. Chocolate,
+-- Rowdy,
+-- Tabitha,
+-- Sergeant Brown,
+-- Melissa,
+-- Grinch,
+-- Wendy
+-- );
